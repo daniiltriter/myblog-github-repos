dc94f29587b80541ad240725f4cb27077d18e76e
Daniil Gruzdev <daniiltriter@gmail.com>
2022-07-06 19:44:10 +0200

    user and role models added. dbContext added

---
+++ Models/ApplicationContex.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Models
+{
+    public class ApplicationContex : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public ApplicationContex(DbContextOptions<ApplicationContex> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
---
+++ Models/Role.cs
@@ -0,0 +1,16 @@
+﻿ namespace MyBlog.Models
+{
+    public class Role
+    {
+        // используем код-конвенцию для настройки БД
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+
+        // отношение один-ко-многим
+        public Role()
+        {
+            Users = new List<User>();
+        }
+    }
+}
