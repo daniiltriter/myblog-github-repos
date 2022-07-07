f014ec441c7a5a3307a8bf3cdbf0418758b7418d
Daniil Gruzdev <daniiltriter@gmail.com>
2022-07-07 07:02:57 +0200

    Edited AccountController.cs Added claim authorization. Added Login and Register views for testing authorization

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using MyBlog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MyBlog.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private ApplicationContext _context;
+        public AccountController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User? user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    user = new User { Email = model.Email, Password = model.Password };
+                    Role? userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+                    if (userRole != null)
+                        user.Role = userRole;
+
+                    _context.Users.Add(user);
+                    await _context.SaveChangesAsync();
+
+                    await Authenticate(user);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Try again!");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+
+        }
+
+        private async Task Authenticate(User user)
+        {
+            throw new NotImplementedException();
         }
     }
 }
