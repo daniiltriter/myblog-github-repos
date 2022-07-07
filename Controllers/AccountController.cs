a484b737bd1ffdd76ae9985b6331e182bef96ad3
Daniil Gruzdev <daniiltriter@gmail.com>
2022-07-07 08:06:54 +0200

    edited AccountController.cs (added login action)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
-using MyBlog.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+
+using System.Security.Claims;
+
+using MyBlog.Models;
 
 namespace MyBlog.Controllers
 {
@@ -57,12 +57,32 @@ namespace MyBlog.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel model)
         {
+            if (ModelState.IsValid)
+            {
+                User? user = await _context.Users
+                    .Include(u => u.Role).FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(user);
 
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Try again");
+            }
+            return View(model);
         }
 
         private async Task Authenticate(User user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
     }
 }
